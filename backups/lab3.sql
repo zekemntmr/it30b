dbb42de1fadefd2ab7d5615e558bb9f994190381
zekemntmr <zekemntmr@gmail.com>
2026-08-27 08:24:58 +0800

    updated schema on lab3.sql

--- backups/lab3.sql
+++ backups/lab3.sql
@@ -18,9 +18,9 @@ create table books(
 insert into borrow(student_id, book_id) values (1,1),(1,2),(3,2);
 
 select br.borrow_id, s.student_id,
-    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name, s.student_course,
+    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name, 
     b.book_title, b.book_author, b.book_category,
-    br.borrow_date 
+    br.borrow_date, br.borrow_return_date
 FROM borrow br
     JOIN students s ON br.student_id = s.student_id
     JOIN books b ON br.book_id = s.student_id
@@ -30,5 +30,25 @@ ALTER TABLE borrow
 MODIFY  borrow_return_date TIMESTAMP NULL DEFAULT NULL;
 
 UPDATE borrow
-set borrow_return_date = NULL
-WHERE borrow_return_date = '2026-08-15 08:30:00';
+SET borrow_return_date = NULL
+WHERE borrow_return_date = '2026-08-25 08:17:03';
+
+-- RETURN A BOOK BY UPDATING THE BORROW_RETURN_DATE TO THE CURRENT TIMESTAMP
+
+UPDATE borrow
+SET borrow_return_date = CURRENT_TIMESTAMP
+--change value of borrow_id to the ID of the borrow record you want to update
+WHERE borrow_id= 1 AND borrow_return_date IS NULL;
+
+select br.borrow_id, s.student_id,
+    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name, 
+    b.book_title, b.book_author, b.book_category,
+    br.borrow_date, br.borrow_return_date
+FROM borrow br
+    JOIN students s ON br.student_id = s.student_id
+    JOIN books b ON br.book_id = b.book_id
+WHERE br.borrow_return_date IS NOT NULL
+ORDER BY br.borrow_date DESC; 
+
+
+
