55e47c1a1d0b12a20a0d884c7123f0cd2dacfc2f
zekemntmr <zekemntmr@gmail.com>
2026-09-17 08:44:20 +0800

    initial frontend progress

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
 //Database Connection
-$host = 'localhost'
-$db = 'it30b_lab_db'
-$user = 'root'
-$pass = ''
+$host = 'localhost';
+$db = 'it30b_lab_db';
+$user = 'root';
+$pass = '';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host; dbname=$db; charset=$charset"
+$dsn = "mysql:host=$host; dbname=$db; charset=$charset";
 
-$option = [
-    PDO::ATTR_ERR => PDO::ERRMODE_EXCEPTION,
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES => false,
-]
+];
 
 try{
-    $pdo = new PDO($dn,$user,$pass,$options);
-    echo 'connection successful';
+    $pdo = new PDO($dsn,$user,$pass,$options);
+    echo 'Connection successful';
 } catch(PDOException $e){
-    die("Database Connection faileed" . $e->getMessage())
+    die("Database Connection failed" . $e->getMessage());
 }
 
 //Session
@@ -28,7 +28,19 @@ session_start();
 $section = $_GET['section'] ??'students';
 
 //Determine CRUD Operation
-$action = $_GET['action'] ??'';
+$action = $_GET['action'] ?? '';
+
+//Fetch Students
+if($section ==='students'){
+
+    $stmt = $pdo->query("
+        SELECT *
+        FROM students
+        ORDER BY student_id DESC
+    ");
+    
+    $students = $stmt->fetchAll();
+}
 
 ?>
 <!DOCTYPE html>
@@ -39,6 +51,62 @@ $action = $_GET['action'] ??'';
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Simple Library System</h1>
+    <nav>
+        <a href="index.php?section=students">Students</a>
+        <a href="index.php?section=books">Books</a>
+        <a href="index.php?section=borrow">Borrow</a>
+    </nav>
+    <hr>
+    <?php if($section === 'students'): ?>
+        <h1>Students</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Course</th>
+                    <th>Created At</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($students as $student): ?>
+                    <tr>
+                        <td>
+                            <?=htmlspecialchars($student['student_id']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_first_name']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_last_name']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_course']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_created_at']) ?>
+                        </td>
+                        <td>
+                            <a>Edit</a>
+
+                            <a>Delete</a>
+                        </td>
+                    </tr>
+                <?php endforeach?>
+            </tbody>
+        </table>
+    <?php endif;?>
+
+    <?php if($section === 'books'): ?>
+        <h1>Books</h1>
+    <?php endif;?>
+
+    <?php if($section === 'borrow'): ?>
+        <h1>Borrow</h1>
+    <?php endif;?>
+
 </body>
 </html>
