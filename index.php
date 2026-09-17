1d363fd7044a42b76978ea660616ae75f2b33b78
zekemntmr <zekemntmr@gmail.com>
2026-09-17 08:01:10 +0800

    initial commit database connection

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+//Database Connection
+$host = 'localhost'
+$db = 'it30b_lab_db'
+$user = 'root'
+$pass = ''
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host; dbname=$db; charset=$charset"
+
+$option = [
+    PDO::ATTR_ERR => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+]
+
+try{
+    $pdo = new PDO($dn,$user,$pass,$options);
+    echo 'connection successful';
+} catch(PDOException $e){
+    die("Database Connection faileed" . $e->getMessage())
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
